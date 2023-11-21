a09a480db52bc9b4a63561ec3a3c9787b29a635d
RahulDev2004 <talktorahul.b@gmail.com>
2023-11-21 18:45:20 +0530

    commited

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { Sidebar } from "./pages/sidebar"
+import { ChatInterface } from "./pages/chatinterface"
 
 function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
+    
+    <ChatInterface/>
     </>
   )
 }
